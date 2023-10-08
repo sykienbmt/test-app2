1d9dd0080c8ac1a1519c3f90466f9f10c604680a
kien.ukpc <kien.nguyen@ukparkingcontrol.com>
2023-10-08 12:09:14 +0700

    init

---
+++ config.js
@@ -0,0 +1,17 @@
+const CONFIG = {
+    titleWeb: "Thiệp mời",
+    introTitle: 'Hello bạn Linh',
+    introDesc: 'Đây là thiệp mời được gửi từ chú Kiên <br> (🥲 đấy là bạn gọi mình thế 🥲 )',
+    btnIntro: 'Vô nội dung chính nè bạn =>>>',
+    title: 'E hèm',
+    desc: 'Chiều tối nay mình xin được phép mời bạn vào lúc 7h tối tại Hẻm cf <br><br> Địa chỉ: 63 Hùng Vương - Thị trấn Quảng Phú <br><br> Ghi chú: không gặp không zìa nhó  ',
+    btnYes: 'Đi nèee <33',
+    btnNo: 'Hông muốn điiii :3',
+    question: 'Cái này mình để bạn nhập cho zui thôi à <3',
+    btnReply: 'Gửi cho bạn <3',
+    reply: 'Thiệt ra ấn gì nó cũng ra câu này thoy chúc bạn may mắn  🤣',
+    mess: 'Đến đây là ổn roài',
+    messDesc: 'Tối nay 7h nhóoooooooooo',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://www.facebook.com/Sykienbmt'
+}
